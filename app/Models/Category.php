48946e97a52e51051f13d5270383e466d188d85a
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-26 17:01:24 +0700

    Introduction Catering App| create migration and model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\CateringPackage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'photo',
+    ];
+
+    public function setNameAttribute($value) {
+        $this->attibutes['name'] = $value;
+        $this->attibutes['slug'] = Str::slug($value);
+    }
+
+    // 1 category memiliki > dari 1 cateringPackages
+    public function cateringPackages(): HasMany {
+        return $this->hasMany(CateringPackage::class);
+    }
+
 }
--- app/Models/CateringPackage.php
+++ app/Models/CateringPackage.php
@@ -2,9 +2,63 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\CateringBonus;
+use App\Models\CateringTestimonial;
+use App\Models\City;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class CateringPackage extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'is_popular',
+        'category_id',
+        'city_id',
+        'kitchen_id',
+    ];
+
+    public function setNameAttribute($value) {
+        $this->attibutes['name'] = $value;
+        $this->attibutes['slug'] = Str::slug($value);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    
+    public function city(): BelongsTo  {
+        return $this->belongsTo(City::class);
+    }
+    
+    public function kitchen(): BelongsTo  {
+        return $this->belongsTo(Kitchen::class);
+    }
+
+    public function photos(): HasMany {
+        return $this->hasMany(CateringPhoto::class);
+    }
+    
+    public function testimonials(): HasMany {
+        return $this->hasMany(CateringTestimonial::class);
+    }
+    
+    public function bonuses(): HasMany {
+        return $this->hasMany(CateringBonus::class);
+    }
+    
+    public function tiers(): HasMany {
+        return $this->hasMany(CateringTier::class);
+    }
 }
