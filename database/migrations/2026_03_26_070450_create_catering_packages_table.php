2823cf221d372ad80c09d45a406a81124c301f36
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-26 14:21:18 +0700

    Introduction Catering App| create migration and model

--- database/migrations/2026_03_26_070450_create_catering_packages_table.php
+++ database/migrations/2026_03_26_070450_create_catering_packages_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('catering_packages', function (Blueprint $table) {
             $table->id();
+            
             $table->string('name');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->boolean('is_popular');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('kitchen_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
