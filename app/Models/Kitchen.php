5e17ac799d42e95a67e46d7df1d55542af5a84ec
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-27 05:41:01 +0700

    Introduction Catering App| create migration and model Kitchen

--- app/Models/Kitchen.php
+++ app/Models/Kitchen.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\CateringPackage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kitchen extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'year',
+        'photo',
+    ];
+
+    public function cateringPackages(): HasMany {
+        return $this->hasMany(CateringPackage::class);
+    }
 }
