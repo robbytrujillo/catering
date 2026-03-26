98581f7458f09ab5d7697f5b49bcd30860d493ba
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-27 05:18:42 +0700

    Introduction Catering App| create migration and model cateringSubscription

--- app/Models/CateringSubscription.php
+++ app/Models/CateringSubscription.php
@@ -2,9 +2,60 @@
 
 namespace App\Models;
 
+use App\Models\CateringPackage;
+use App\Models\CateringTier;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CateringSubscription extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'booking_trx_id',
+        'name',
+        'phone',
+        'email',
+        'delivery_time',
+        'proof',
+        'post_code',
+        'city',
+        'notes',
+        'address',
+        'total_amount',
+        'price',
+        'duration',
+        'quantity',
+        'total_tax_amount',
+        'is_paid',
+        'started_at',
+        'ended_at',
+        'catering_package_id',
+        'catering_tier_id',
+    ];
+
+    protected $casts = [
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
+
+    public static function generateUniqueTrxId() {
+        $prefix = 'CATERING';
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomString)->exists());
+
+        return $randomString;
+    }
+
+    public function cateringPackage(): BelongsTo {
+        return $this->belongsTo(CateringPackage::class, 'catering_package_id');
+    }
+    
+    public function cateringTier(): BelongsTo {
+        return $this->belongsTo(CateringTier::class, 'catering_tier_id');
+    }
 }
