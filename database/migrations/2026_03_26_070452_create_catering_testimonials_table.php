1450e3507146e8b2152bea62922aaa9cbc35cd41
robbytrujillo <robbyikusuma88@gmail.com>
2026-03-26 14:34:10 +0700

    Introduction Catering App| create migration and model catering testimonials

--- database/migrations/2026_03_26_070452_create_catering_testimonials_table.php
+++ database/migrations/2026_03_26_070452_create_catering_testimonials_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('catering_testimonials', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('message');
+            $table->string('photo');
+            $table->foreignId('catering_package_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
