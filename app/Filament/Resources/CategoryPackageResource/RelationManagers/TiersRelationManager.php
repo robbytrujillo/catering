263a689a3ac1b38ac59fa5c36fa512c263db6b7e
robbytrujillo <robbyikusuma88@gmail.com>
2026-05-30 08:20:05 +0700

    Relation Manager Catering Package Resource tiers name

--- app/Filament/Resources/CategoryPackageResource/RelationManagers/TiersRelationManager.php
+++ app/Filament/Resources/CategoryPackageResource/RelationManagers/TiersRelationManager.php
@@ -21,6 +21,38 @@ class TiersRelationManager extends RelationManager
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                
+                Forms\Components\FileUpload::make('photo')
+                    ->image()
+                    ->required(),
+
+                Forms\Components\TextInput::make('tagline')
+                    ->required()
+                    ->maxLength(255),
+                
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+                
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric()
+                    ->prefix('People'),
+                
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+                
+                Forms\Components\Repeater::make('benefits')
+                    ->relationship('benefits')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                    ])
+                    ,
             ]);
     }
 
