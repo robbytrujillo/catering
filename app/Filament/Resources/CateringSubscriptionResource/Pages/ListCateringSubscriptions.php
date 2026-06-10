b16a288ef89060d558730367c9ba5c070cfb6d8b
robbytrujillo <robbyikusuma88@gmail.com>
2026-06-11 03:31:35 +0700

    Custom Dashboard Transaction

--- app/Filament/Resources/CateringSubscriptionResource/Pages/ListCateringSubscriptions.php
+++ app/Filament/Resources/CateringSubscriptionResource/Pages/ListCateringSubscriptions.php
@@ -5,10 +5,18 @@ namespace App\Filament\Resources\CateringSubscriptionResource\Pages;
 use App\Filament\Resources\CateringSubscriptionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\CateringSubscriptionResource\Widgets\CateringSubscriptionStats;
 
 class ListCateringSubscriptions extends ListRecords
 {
     protected static string $resource = CateringSubscriptionResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            CateringSubscriptionStats::class,
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
