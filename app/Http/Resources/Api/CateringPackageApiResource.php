06b37e0d040eebd73103bf982eb9acadc25bf2de
robbytrujillo <robbyikusuma88@gmail.com>
2026-06-14 06:08:40 +0700

    Create API resource dan routing

--- app/Http/Resources/Api/CateringPackageApiResource.php
+++ app/Http/Resources/Api/CateringPackageApiResource.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Models\Category;
+use App\Models\CateringBonus;
+use App\Models\CateringPhoto;
+use App\Models\CateringTestimonial;
+use App\Models\CateringTier;
+use App\Models\City;
+use App\Models\Kitchen;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +21,22 @@ class CateringPackageApiResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'is_popular' => $this->is_popular,
+            'thumbnail' => $this->thumbnail,
+            'about' => $this->about,
+            'city' => new City($this->whenLoaded('city')),
+            'category' => new Category($this->whenLoaded('category')),
+            'kitchen' => new Kitchen($this->whenLoaded('kitchen')),
+            'photos' => CateringPhoto::collection($this->whenLoaded('photos')),
+            'bonuses' => CateringBonus::collection($this->whenLoaded('bonuses')),
+            'testimonials' => CateringTestimonial::collection($this->whenLoaded('testimonials')),
+            'tiers' => new CateringTier($this->whenLoaded('tiers')),
+
+        ];
     }
 }
