df7ff7c585ee0c566429e6a55561246197ec622d
robbytrujillo <robbyikusuma88@gmail.com>
2026-06-27 16:51:02 +0700

    Create API resource dan routing

--- app/Http/Resources/Api/CateringSubscriptionApiResource.php
+++ app/Http/Resources/Api/CateringSubscriptionApiResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Http\Resources\Api\CateringPackageApiResource;
+use App\Http\Resources\Api\CateringTierApiResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +16,29 @@ class CateringSubscriptionApiResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'post_code' => $this->post_code,
+            'city' => $this->city,
+            'address' => $this->address,
+            'notes' => $this->notes,
+            'started_at' => $this->started_at,
+            'ended_at' => $this->ended_at,
+            'booking_trx_id' => $this->booking_trx_id,
+            'price' => $this->price,
+            'total_tax_amount' => $this->total_tax_amount,
+            'total_amount' => $this->total_amount,
+            'delivery_time' => $this->delivery_time,
+            'quantity' => $this->quantity,
+            'duration' => $this->duration,
+            'isPaid' => $this->is_paid,
+            'proof' => $this->proof,
+            'cateringPackage' => new CateringPackageApiResource($this->whenLoaded('cateringPackage')),
+            'cateringTier' => new CateringTierApiResource($this->whenLoaded('cateringTier')),
+        ];
     }
 }
