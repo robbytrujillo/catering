8a82c6420eaf25e6a28565fc91a5c32b845e586b
robbytrujillo <robbyikusuma88@gmail.com>
2026-06-29 20:34:35 +0700

    Create API resource dan routing

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,19 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/catering-packages/{cateringPackage:slug}', [CateringPackageController::class, 'show']);
 Route::apiResource('/catering-packages', CateringPackageController::class);
+
+Route::get('/filters/catering-packages/', [CateringController::class, 'filterPackages']);
+
+Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
+Route::apiResource('/categories', CategoryController::class);
+
+oute::get('/category/{category:slug}', [CategoryController::class, 'show']);
+Route::apiResource('/categories', CategoryController::class);
+
+oute::get('/city/{city:slug}', [CityController::class, 'show']);
+Route::apiResource('/city', CityController::class);
+
+Route::apiResource('/testimonials', CateringTestimonialController::class);
+
+Route::post('/booking-transaction', [CateringSubscriptionPackageController::class, 'store']);
+Route::post('/check-booking', [CateringSubscriptionPackageController::class, 'booking_details']);
