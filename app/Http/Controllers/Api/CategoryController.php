0975ff81c16a4859fc280fcaabccc72d7e090914
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-07 09:30:49 +0700

    City category controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CategoryApiResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     //
+    public function index() {
+        $categories = Category::with(['cateringPackages'])->get();
+        return CategoryApiResource::collection($categories);
+    }
+
+    public function show(Category $category) { // jakarta-selatan, surabaya
+        $category->load(['cateringPackages', 'cateringPackages.city', 'cateringPackages.tiers']);
+
+        $category->loadCount('cateringPackages');
+
+        return new CategoryApiResource($category);
+    }
 }
--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityApiResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     //
+    public function index() {
+        $cities = City::with(['cateringPackages'])->get();
+        return CityApiResource::collection($cities);
+    }
+
+    public function show(City $city) { // jakarta-selatan, surabaya
+        $city->load(['cateringPackages', 'cateringPackages.category', 'cateringPackages.tiers']);
+
+        $city->loadCount('cateringPackages');
+
+        return new CityApiResource($city);
+    }
 }
