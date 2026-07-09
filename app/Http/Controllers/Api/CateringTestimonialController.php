f4854a3ca69ebf90c629e7dd156f48fdb42e9a1e
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-09 14:56:08 +0700

    City category controller

--- app/Http/Controllers/Api/CateringTestimonialController.php
+++ app/Http/Controllers/Api/CateringTestimonialController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CateringTestimonialApiResource;
+use App\Models\CateringTestimonial;
 use Illuminate\Http\Request;
 
 class CateringTestimonialController extends Controller
 {
     //
+    public function index() {
+        $testimonials = CateringTestimonial::with(['cateringPackage'])->get();
+        return CateringTestimonialApiResource::collection($testimonials);
+    }
 }
