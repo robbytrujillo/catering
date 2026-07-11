6333548414558dd3bc63dbec19174d9420202495
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-12 06:32:02 +0700

    Catering dan subscribe controller

--- app/Http/Controllers/Api/CateringPackageController.php
+++ app/Http/Controllers/Api/CateringPackageController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CateringPackageApiResource;
+use App\Models\CateringPackage;
 use Illuminate\Http\Request;
 
 class CateringPackageController extends Controller
 {
     //
     public function index() {
-        $packages = CateringPackage::with(['cateringTestimonial'])->get();
-        return CateringPackageApiResource::collection($packages);
+        $cateringPackages = CateringPackage::with(['city', 'kitchen', 'category', 'tiers', 'tiers.benefits'])->get();
+        return CateringPackageApiResource::collection($cateringPackages);
+    }
+
+    public function show(CateringPackage $cateringPackage) {
+        $cateringPackage->load(['city', 'photos', 'bonuses', 'category', 
+            'kitchen', 'testimonials', 'tiers', 'tiers.benefits'
+        ]);
+
+        $cateringPackage->kitchen->loadCount('cateringPackahes');
+
+        return new CateringPackageApiResource($cateringPackage);
     }
 }
