09531a863c322303d5dde314b3f07a47f7ed6cba
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-25 09:29:46 +0700

    Catering dan subscribe controller

--- app/Http/Controllers/Api/CateringSubscriptionController.php
+++ app/Http/Controllers/Api/CateringSubscriptionController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\CateringPackage;
 use Illuminate\Http\Request;
 
 class CateringSubscriptionController extends Controller
 {
     //
     public function store(StoreCateringSubscribeRequest $request) {
+        $validatedData = $request->validated();
 
+        $cateringPackage = CateringPackage::find($validatedData['catering_package_id']);
+
+        if (!$cateringPackage) {
+            return response()->json(['message' => 'Tier package not found, please choose the existings tiers available']);
+        }
     }
 }
